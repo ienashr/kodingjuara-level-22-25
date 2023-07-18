ac9905e4401373232f35930358116ea7df33025f
ienashr <ienasharisah@gmail.com>
2023-07-18 11:02:13 +0700

    styling form

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
-  const fetchData = () => {
-            fetch('https://directus-ienas.cloud.programmercepat.com/items/news')
-                .then(response => response.json())
-                .then(data => {
-                    const container = document.getElementById('data-container');
-                    container.innerHTML = '';
+fetch('https://directus-ienas.cloud.programmercepat.com/items/news')
+  .then(response => response.json()) // Parse the response as JSON
+  .then(data => {
+    // Access the retrieved data here
+    console.log(data.data);
 
-                    data.forEach(news => {
-                        const newsElement = document.createElement('div');
-                        newsElement.innerHTML = `
-                  <h3>${news.title}</h3>
-                  <p>${news.description}</p>
-                `;
-                        container.appendChild(newsElement);
-                    });
-                })
-                .catch(error => {
-                    console.error('Error:', error);
-                });
-        };
+    // Display the data in the UI
+    const container = document.getElementById('data-container');
+
+          // Populate the ID dropdown in the update form
+    const updateFormDropdown = document.getElementById('news-id-update');
+      updateFormDropdown.innerHTML = '<option value="" disabled selected>Select News ID</option>';
+      data.data.forEach(news => {
+        const option = document.createElement('option');
+        option.value = news.id;
+        option.textContent = news.id;
+        updateFormDropdown.appendChild(option);
+      });
+
+       // Populate the ID dropdown in the delete form
+      const deleteFormDropdown = document.getElementById('news-id-delete');
+      deleteFormDropdown.innerHTML = '<option value="" disabled selected>Select News ID</option>';
+      data.data.forEach(news => {
+        const option = document.createElement('option');
+        option.value = news.id;
+        option.textContent = news.id;
+        deleteFormDropdown.appendChild(option);
+      });
+
+    
+    // Iterate over the data and create HTML elements to display it
+    data.data.forEach(news => {
+      const newsElement = document.createElement('div');
+      newsElement.classList.add('news-wrapper'); // Add the news wrapper class
+
+      newsElement.textContent = news.title;
+      newsElement.textContent = news.description;
+      container.appendChild(newsElement);
+    });
+  })
+  .catch(error => {
+    // Handle any errors that occurred during the request
+    console.error('Error:', error);
+  });
+
 
         document.getElementById('create-form').addEventListener('submit', function (event) {
             event.preventDefault();
@@ -46,3 +71,77 @@
                     console.error('Error:', error);
                 });
         });
+
+        document.getElementById('update-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      const newsId = document.getElementById('news-id-update').value;
+      const title = document.getElementById('news-title-update').value;
+      const description = document.getElementById('news-description-update').value;
+
+      const data = {
+        title: title,
+        description: description
+      };
+
+      fetch(`https://directus-ienas.cloud.programmercepat.com/items/news/${newsId}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+        .then(response => response.json())
+        .then(responseData => {
+          console.log('Response:', responseData);
+          fetchData(); // Refresh the data after updating the news
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+        });
+    
+          document.getElementById('delete-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      const newsId = document.getElementById('news-id-delete').value;
+
+      fetch(`https://directus-ienas.cloud.programmercepat.com/items/news/${newsId}`, {
+        method: 'DELETE'
+      })
+        .then(response => {
+          if (response.ok) {
+            console.log('News deleted successfully');
+            fetchData(); // Refresh the data after deleting the news
+          } else {
+            console.error('Error deleting news:', response.statusText);
+          }
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    });
+
+document.getElementById('delete-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      const newsId = document.getElementById('news-id-delete').value;
+
+      fetch(`https://directus-ienas.cloud.programmercepat.com/items/news/${newsId}`, {
+        method: 'DELETE'
+      })
+        .then(response => {
+          if (response.ok) {
+            console.log('News deleted successfully');
+            fetchData(); // Refresh the data after deleting the news
+          } else {
+            console.error('Error deleting news:', response.statusText);
+          }
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    });   
+
+// Fetch initial data on page load
+    fetchData();
